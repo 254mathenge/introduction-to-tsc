323fbfdca3beeeee28f5da0e1c30d32bea4f93a9
254mathenge <mathengefaith49@gmail.com>
2024-10-07 03:03:19 +0300

    adds boolean type

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 console.log("I Love TypeScript");
-function add(n1, n2) {
-    if (typeof n1 !== "number" || typeof n2 !== "number") {
-        throw new Error("Invalid input");
-    }
-    return n1 + n2;
+function add(n1, n2, showResult) {
+    // if (typeof n1 !== "number"  || typeof n2 !== "number") {
+    //     throw new Error("Invalid input")result = n1 + n2;
+    var result = n1 + n2;
+    showResult ? console.log(result) : null;
+    // if (showResult){
+    // console.log(result)
+    // }
+    // else {
+    // return "invalid";
+    // }
 }
-var number1 = "5";
+var number1 = 5;
 var number2 = 2.5;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+console.log(add(number1, number2, printResult));
--- app.ts
+++ app.ts
@@ -1,13 +1,19 @@
 console.log("I Love TypeScript");
 
-function add(n1:number , n2:number){
-    if (typeof n1 !== "number"  || typeof n2 !== "number") {
-        throw new Error("Invalid input");
-    }
-    return n1 + n2;
+function add(n1:number,n2:number,showResult:boolean){
+    // if (typeof n1 !== "number"  || typeof n2 !== "number") {
+    //     throw new Error("Invalid input")result = n1 + n2;
+const result =n1+n2;
 
+showResult ?console.log(result): null;
+// if (showResult){
+// console.log(result)
+// }
+// else {
+// return "invalid";
+// }
 }
 const number1=5;
 const number2=2.5;
-const result=add(number1,number2);
-console.log(result);
+const printResult =true;
+console.log(add(number1,number2,printResult));
