ab9f9e73edd63b142bd23320f7b967636376f3b6
254mathenge <mathengefaith49@gmail.com>
2024-10-09 00:24:25 +0300

    adds literal types

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,9 +6,18 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
+    if (resultConversion === "as-number") {
+        return +result;
+    }
+    else {
+        result = result.toString();
+    }
     return result;
 }
-var combinedAges = combine(30, 36);
-console.log(combinedAges);
-var combinedString = combine('hello', 'world');
-console.log(combinedString);
+//     }
+// const combinedAges =combine(30,36,"as-number");
+// console.log(combinedAges);
+// const combinedString = combine('hello','world',"as-text");
+// console.log(combinedString)
+var combinedStringAges = combine("30", "36", "as-number");
+console.log(combinedStringAges);
--- app.ts
+++ app.ts
@@ -1,14 +1,24 @@
-function combine(input1:number|string ,input2:number|string){
+function combine(input1:number|string ,input2:number|string,
+resultConversion:string
+){
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number'){
         result = input1 + input2;
     }else{
         result =input1.toString() + input2.toString();
     }
-    return result;
+    if (resultConversion === "as-number"){
+        return+result;
+    }else{
+        result = result.toString();
     }
-const combinedAges =combine(30,36);
-console.log(combinedAges);
+    return result;
+}
+//     }
+// const combinedAges =combine(30,36,"as-number");
+// console.log(combinedAges);
+// const combinedString = combine('hello','world',"as-text");
+// console.log(combinedString)
 
-const combinedString = combine('hello','world');
-console.log(combinedString);
+const combinedStringAges=combine("30","36","as-number")
+console.log(combinedStringAges);
