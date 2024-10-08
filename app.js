560659df9887e3b92bee44758fd6a4358a2975b8
254mathenge <mathengefaith49@gmail.com>
2024-10-08 22:58:57 +0300

    commit

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 // const person:
-// object
 // {
 //     name:string ,
 //     age:number,
+//     hobbies:string[],
+//     role:[number,string]
 // }
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "faith",
     age: 25,
-    hobbies: ["swimming", "reading"]
+    hobbies: ["swimming", "reading"],
+    role: Role.ADMIN
 };
+// tuples
+// person.role.push("admin")
 // let favoriteActivities:string[];
 // favoriteActivities=["swimming"]
 // const favoriteActivities :string[] =["swimming","reading"]
-console.log(person);
+// console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
+}
--- app.ts
+++ app.ts
@@ -1,14 +1,21 @@
 // const person:
-// object
 // {
 //     name:string ,
 //     age:number,
+//     hobbies:string[],
+//     role:[number,string]
+
 // }
+enum Role{ADMIN="ADMIN",READ_ONLY=100,AUTHOR=200}
+
 const person={
     name:"faith",
     age:25,
-    hobbies:["swimming","reading"]
+    hobbies:["swimming","reading"],
+    role:Role.ADMIN
 };
+// tuples
+// person.role.push("admin")
 // let favoriteActivities:string[];
 // favoriteActivities=["swimming"]
 
@@ -16,4 +23,8 @@ const person={
 // console.log(person);
 for(const hobby of person.hobbies){
 console.log(hobby.toUpperCase())
+}
+
+if (person.role===Role.ADMIN){
+console.log("is admin");
 }
